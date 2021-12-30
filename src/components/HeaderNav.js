db3c90856a7440657790af4e2420cfcade0a6945
Jonher16 <80284536+Jonher16@users.noreply.github.com>
2021-12-30 09:42:41 +0100

    functional everythin

---
+++ src/components/HeaderNav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "../App.scss"
+
+const HeaderNav = () => {
+  return (
+    <div>
+      <Navbar bg="mediumgray" width="100%" variant="dark">
+     
+          <Navbar.Brand><h1>FlexDrone</h1></Navbar.Brand>
+         
+        
+      </Navbar>
+    </div>
+  );
+};
+
+export default HeaderNav;
